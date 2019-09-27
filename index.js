a11351db6d8db7163f55652b41d09ecb071db895
Spencer Vaterlaus <spencervaterlaus@gmail.com>
2019-09-26 21:16:15 -0500

    Various updates:
    - add conditional checks to orchestrator (and Subject)
    - use splice instead of spread operator in cancel (performance)
    - set _running to false after cancel
    - add Observable creation helpers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const util = require('./util')
+const { hasProp } = require('./util')
 
 const validateObserver = (observer) => {
-  if (!util.hasProp(observer, 'next')) {
+  if (!hasProp(observer, 'next')) {
     throw Error('observer requires a "next" method.')
   } else if (typeof observer.next !== 'function') {
     throw Error(`observer.next must be a Function. Type: ${typeof observer.next}`)
@@ -13,7 +13,7 @@ const validateObserver = (observer) => {
 }
 
 const validateProducer = (producer) => {
-  if (!util.hasProp(producer, 'start')) {
+  if (!hasProp(producer, 'start')) {
   } else if (typeof producer.start !== 'function') {
     throw Error(`producer.start must be a Function. Type: ${typeof producer.start}`)
   } else if (producer.stop && typeof producer.stop !== 'function') {
@@ -45,9 +45,25 @@ function Observable (producer) {
     [_observers]: [],
     [_producer]: producer,
     [_orchestrator]: {
-      next: (e) => { self[_observers].forEach((observer) => { observer.next(e) }) },
-      error: (e) => { self[_observers].forEach((observer) => { observer.error(e) }) },
-      complete: (e) => { self[_observers].forEach((observer) => { observer.complete(e) }) }
+      next: (e) => {
+        self[_observers].forEach((observer) => {
+          observer.next(e)
+        })
+      },
+      error: (err) => {
+        self[_observers].forEach((observer) => {
+          if (observer.error) {
+            observer.error(err)
+          } else {
+            throw err
+          }
+        })
+      },
+      complete: () => {
+        self[_observers].forEach((observer) => {
+          if (observer.complete) observer.complete()
+        })
+      }
     },
 
     observe (observer) {
@@ -69,23 +85,87 @@ function Observable (producer) {
       validateID(id)
       const targetIndex = self[_observers].findIndex(observer => observer._id === id)
       if (targetIndex === -1) throw new Error(`observer not found. ID: ${id}`)
-      self[_observers] = [
-        ...self[_observers].slice(0, targetIndex),
-        ...self[_observers].slice(targetIndex + 1, self[_observers].length)
-      ]
-      if (self[_observers].length === 0 && self[_producer].stop) self[_producer].stop()
+      self[_observers].splice(targetIndex)
+      if (!self[_observers].length && self[_producer].stop) {
+        self[_running] = false
+        self[_producer].stop()
+      }
       return id
     },
     cancelAll () {
       const ids = self[_observers].map(observer => observer.id)
       self[_observers] = []
-      if (self[_producer].stop) self[_producer].stop()
+      if (self[_producer].stop) {
+        self[_running] = false
+        self[_producer].stop()
+      }
       return ids
     }
   }
 
   return self
 }
+
+Observable.from = (...values) => Observable({
+  start (observer) {
+    values.forEach((value, i) => {
+      setTimeout(() => {
+        observer.next(value)
+        if (i === values.length - 1) observer.complete()
+      }, 0)
+    })
+  }
+})
+
+Observable.fromTimeout = (delay, value) => Observable({
+  start (observer) {
+    this.observer = observer
+    this.id = setTimeout(() => {
+      observer.next(value)
+      observer.complete()
+    }, delay)
+  },
+  stop () {
+    clearTimeout(this.id)
+    this.observer.complete()
+  }
+})
+
+Observable.fromInterval = (delay) => Observable({
+  start (observer) {
+    this.observer = observer
+    let n = 1
+    this.id = setInterval(() => {
+      observer.next(n)
+      n += 1
+    }, delay)
+  },
+  stop () {
+    clearInterval(this.id)
+    this.observer.complete()
+  }
+})
+
+Observable.fromPromise = (promise) => Observable({
+  async start (observer) {
+    try {
+      observer.next(await promise)
+      observer.complete()
+    } catch (err) {
+      observer.error(err)
+    }
+  }
+})
+
+Observable.fromEvent = (target, eventName) => Observable({
+  start (observer) {
+    this.observer = observer
+    target.addEventListener(eventName, observer.next)
+  },
+  stop () {
+    target.removeEventListener(eventName, this.observer.next)
+  }
+})
 
 function Subject (initialValue) {
   const hasInitialValue = !!arguments.length
@@ -96,9 +176,25 @@ function Subject (initialValue) {
     [_nonce]: 0,
     [_observers]: [],
 
-    next: (e) => { self[_observers].forEach((observer) => { observer.next(e) }) },
-    error: (e) => { self[_observers].forEach((observer) => { observer.error(e) }) },
-    complete: (e) => { self[_observers].forEach((observer) => { observer.complete(e) }) },
+    next: (e) => {
+      self[_observers].forEach((observer) => {
+        observer.next(e)
+      })
+    },
+    error: (err) => {
+      self[_observers].forEach((observer) => {
+        if (observer.error) {
+          observer.error(err)
+        } else {
+          throw err
+        }
+      })
+    },
+    complete: () => {
+      self[_observers].forEach((observer) => {
+        if (observer.complete) observer.complete()
+      })
+    },
     observe (observer) {
       validateObserver(observer)
       const id = self[_nonce]
@@ -115,10 +211,7 @@ function Subject (initialValue) {
       validateID(id)
       const targetIndex = self[_observers].findIndex(observer => observer._id === id)
       if (targetIndex === -1) throw new Error(`observer not found. ID: ${id}`)
-      self[_observers] = [
-        ...self[_observers].slice(0, targetIndex),
-        ...self[_observers].slice(targetIndex + 1, self[_observers].length)
-      ]
+      self[_observers].splice(targetIndex)
       return id
     },
     cancelAll () {
@@ -126,7 +219,6 @@ function Subject (initialValue) {
       self[_observers] = []
       return ids
     }
-
   }
 
   return self
